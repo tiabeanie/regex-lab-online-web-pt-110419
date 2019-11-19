7afd4ab9a5e75b802664d3f47fee4f00a1407e2a
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-19 19:01:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,11 +15,10 @@ def words_five_letters_long(text)
   return_array.grep(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  first_letter = text.scan(/\w+/)[0]
-  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
+def first_word_capitalized_and_ends_with_punctuation_2?(text)
+  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
     true
-  else  
+  else 
     false
   end
 end
