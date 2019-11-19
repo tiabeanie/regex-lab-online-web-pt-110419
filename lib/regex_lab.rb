b797dacd7c09bd3cd2990eb3e79dbe3d6e321f30
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-19 19:03:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
   return_array.grep(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation_2?(text)
+def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z]/) && text.match(/[\.]$/)
     true
   else 
